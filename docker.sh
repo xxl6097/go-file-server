e7963e1d7a2024ce8974bdc7da0806c0e522a957
MacBookAirM2 <uuxia@macbookairm2.com>
2024-07-22 20:23:52 +0800

    release v0.0.0

--- docker.sh
+++ docker.sh
@@ -6,13 +6,13 @@ appdir="./cmd/app"
 
 function getversion() {
   version=$(cat version.txt)
-  if [ "$appversion" = "" ]; then
+  if [ "$version" = "" ]; then
     version="0.0.0"
-    echo $appversion
+    echo $version
   else
-    v3=$(echo $appversion | awk -F'.' '{print($3);}')
-    v2=$(echo $appversion | awk -F'.' '{print($2);}')
-    v1=$(echo $appversion | awk -F'.' '{print($1);}')
+    v3=$(echo $version | awk -F'.' '{print($3);}')
+    v2=$(echo $version | awk -F'.' '{print($2);}')
+    v1=$(echo $version | awk -F'.' '{print($1);}')
     if [[ $(expr $v3 \>= 99) == 1 ]]; then
       v3=0
       if [[ $(expr $v2 \>= 99) == 1 ]]; then
