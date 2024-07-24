dcb7e11f2d452d8ff5fc4352347869bf158413cb
MacBookAirM2 <uuxia@macbookairm2.com>
2024-07-24 19:49:42 +0800

    release v0.5.25

--- build.sh
+++ build.sh
@@ -38,8 +38,7 @@ function getversion() {
 function build_linux_mips_opwnert_REDMI_AC2100() {
   distDir=./dist/${appname}_v${version}_linux_mipsle
   CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -ldflags "$ldflags -s -w -linkmode internal" -o ${distDir} ${appdir}
-  files+="${distDir} "
-  echo "编译完成 ${files}"
+  echo "编译完成 ${distDir}"
 #  bash <(curl -s -S -L http://uuxia.cn:8087/up) ./dist/${appname}_v${version}_linux_mipsle soft/linux/mipsle/${appname}/${version}
 }
 
@@ -48,8 +47,7 @@ function build() {
   arch=$2
   distDir=./dist/${appname}_v${version}_${os}_${arch}
   CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -ldflags "$ldflags -s -w -linkmode internal" -o ${distDir} ${appdir}
-  files+="${distDir} "
-  echo "编译完成 ${files}"
+  echo "编译完成 ${distDir}"
 }
 
 function build_win() {
@@ -59,16 +57,14 @@ function build_win() {
   go generate ${appdir}
   CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -ldflags "$ldflags -s -w -linkmode internal" -o ${distDir} ${appdir}
   rm -rf ${appdir}/resource.syso
-  files+="${distDir} "
-  echo "编译完成 ${files}"
+  echo "编译完成 ${distDir}"
 }
 
 
 function build_windows_arm64() {
   distDir=./dist/${appname}_${version}_windows_arm64.exe
   CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags "$ldflags -s -w -linkmode internal" -o ${distDir} ${appdir}
-  files+="${distDir} "
-  echo "编译完成 ${files}"
+  echo "编译完成 ${distDir}"
 }
 
 function build_menu() {
@@ -92,8 +88,7 @@ for file in ./dist/*; do
         array+="$file "
     fi
 done
-  echo "==>$array"
-  if [ -n "$files" ]; then
+  if [ -n "$array" ]; then
       bash <(curl -s -S -L http://uuxia.cn:8087/up) $files soft/${appname}/${version}
   fi
 
